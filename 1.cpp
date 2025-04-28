b454e9ffce49d6573bda8228668662b43857e860
Maruf <mdmarufsheikh707@gmail.com>
2025-04-28 22:01:34 +0600

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for(int i = 0; i < nums.size(); i++) {
+            for(int j = i + 1; j < nums.size(); j++) {
+                if(nums[i] + nums[j] == target) {
+                    return {i, j};
+                }
+            }
+        }
+        return {};
+    }
+};
+
+int main() {
+    int n, target;
+    cin >> n >> target;
+    vector<int> nums(n);
+    for(int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    Solution solution;
+    vector<int> result = solution.twoSum(nums, target);
+
+    if(!result.empty()) {
+        cout << "[" << result[0] << ", " << result[1] << "]" << endl;
+    } else {
+        cout << "No solution found" << endl;
+    }
+
+    return 0;
+}
